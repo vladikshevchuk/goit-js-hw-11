ff502eeccfab7fdf973541d8f934539975b3d0f6
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2022-12-30 17:07:09 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import PhotoApiService from './js/api-service';
+import templatePhoto from './template/photo-card.hbs';
 import { refs } from "./js/refs";
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const photoApiServis = new PhotoApiService();
 
@@ -16,12 +20,26 @@ function onSerch(evt) {
     }
     photoApiServis.cleanHTML();
     photoApiServis.resetPage();
-    photoApiServis.searchPhoto();
+    photoApiServis.searchPhoto().then(photos => {
+        refs.gallery.insertAdjacentHTML('beforeend', templatePhoto(photos));
+        const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: '250ms',
+        });
+    });
+
     refs.btnMore.classList.remove('is-hidden');
 }
 
 function onLoadMore() {
-    photoApiServis.searchPhoto();
+    photoApiServis.searchPhoto().then(photos => {
+        refs.gallery.insertAdjacentHTML('beforeend', templatePhoto(photos));
+        const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: '250ms',
+        });
+    });
+    
+    var test = $('.gallery a').simpleLightbox();
+    test.refresh();
     
     if (photoApiServis.pageNum > 13) {
         refs.btnMore.classList.add('is-hidden');
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,11 +1,7 @@
 const axios = require('axios').default;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { data } from "./data-files";
-import templateFunction from '../template/photo-card.hbs';
 import { refs } from "../js/refs";
-import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
 
 export default class PhotoApiService {
     constructor() {
@@ -15,20 +11,20 @@ export default class PhotoApiService {
 
     async searchPhoto() {
         try {
-            const response = await axios.get(`${data.BASE_URL}?key=${data.KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`);
-            console.log(response);
-            if (response.data.totalHits === 0) {
-                Notify.success('Sorry, there are no images matching your search query. Please try again.');
-            } else {
-                Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
-                refs.gallery.insertAdjacentHTML('beforeend', templateFunction(response.data.hits));
-                const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250ms' });
-            }
-            
-            
+        const response = await axios.get(`${data.BASE_URL}?key=${data.KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`);
+        // console.log(response);
+        if (response.data.totalHits === 0) {
+            Notify.success(
+            'Sorry, there are no images matching your search query. Please try again.'
+            );
+        } else {
+            Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
             this.incrementPage();
+            const photos = await response.data.hits;
+            return photos;
+        }
         } catch (error) {
-            console.error(error);  
+        console.error(error);
         }
     }
 
